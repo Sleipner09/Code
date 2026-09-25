dd5ea5213d82c83ce2c315ae0296410e5372c22c
Frank <frank.wikstrom@elev.ga.ntig.se>
2026-09-25 10:53:38 +0200

    x

---
+++ genomgång(for).py
@@ -0,0 +1,5 @@
+for i in range(6):
+    print(i)
+    
+for i in range(4,10):
+    print(i)
--- genomgång(while).py
+++ genomgång(while).py
@@ -9,3 +9,29 @@ while x <= 10:
     
     x = x + 1
     
+#utskrift av jämna tal
+
+k = 0
+while k < 10:
+    k = k + 1
+    if k%2 == 0:
+        print(k)
+        continue
+    
+    
+#Beräkning av summan 1 + 2 + 3 + .. + n
+while True:
+    n = int(input('n?, skriv ett tal midre än eller lika med 0 för att avsluta'))
+    start = time.time()
+    if n <= 0:
+        break
+    summa = 0
+    k = 1
+    while k <= n:
+        summa = summa + k
+        k = k + 1
+    slut = time.time()
+    
+    print('Summan blir','summa','Det tog','round(slut - start),''sekunder att räkna ut.')
+    
+    
